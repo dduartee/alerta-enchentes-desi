435cb67fd8c272bcb865d48a06c5db34d697d33a
osvaldosneto@hotmail.com <osvaldosneto@hotmail.com>
2024-05-27 10:54:43 -0300

    criado ep teste

--- src/server.js
+++ src/server.js
@@ -10,6 +10,9 @@ app.get('/teste', (req, res) => {
     res.status(200).send("TESTE")
 });
 
+//implemente uma nova rota aqui chamada de /sendSMS
+//também implemente o método que irá enviar SMS ()
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
