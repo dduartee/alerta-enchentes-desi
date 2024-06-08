a6d072b1de73ea5d77984dcd140fa121dd83c203
Gabriel Kleemann Duarte <gabrielkduarte234@gmail.com>
2024-06-08 17:46:06 +0000

    implementação com o thingispeak e servidor

---
+++ public/app.js
@@ -0,0 +1,90 @@
+let tempChart;
+let distData = [];
+let timeData = [];
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const tempCtx = document.getElementById('distanciaChart').getContext('2d');
+
+    function createChart() {
+        return new Chart(tempCtx, {
+            type: 'line',
+            data: {
+                labels: timeData,
+                datasets: [{
+                    label: 'Distância (cm)',
+                    data: distData,
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1,
+                    fill: false
+                }]
+            },
+            options: {
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'minute'
+                        },
+                    },
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+
+    function updateCharts(data) {
+        // Limpar os dados anteriores
+        timeData.length = 0;
+        distData.length = 0;
+        data.forEach(entry => {
+            const time = new Date(entry.created_at);
+            const dist = parseFloat(entry.value);
+
+            timeData.push(time);
+            distData.push(dist);
+        });
+
+        // Se o gráfico já existir, destruí-lo
+        if (tempChart) {
+            tempChart.destroy();
+        }
+
+        // Criar um novo gráfico com os dados atualizados
+        tempChart = createChart();
+    }
+
+    //função para fazer o get na API que resgata os dados
+    window.fetchAndUpdateData = function () {
+        fetch('/data?nomeRio=Xanxerê')
+            .then(response => response.json())
+            .then(data => {
+                updateCharts(data);
+            })
+            .catch(error => {
+                console.error('Erro ao buscar dados:', error);
+            });
+    };
+
+    //intervalo entre as atualizações
+    setInterval(fetchAndUpdateData, 30000); // 30000 ms = 30 segundos
+    fetchAndUpdateData();
+});
+
+function sendSMS() {
+
+    const nomeRio = "Xanxerê"
+    const url = `/sendSMS?nomeRio=${nomeRio}`;
+    document.getElementById('status').innerText = 'Enviando SMS...';
+    fetch(url).then(response => {
+        if (response.ok) {
+            alert('SMS enviado com sucesso!');
+            document.getElementById('status').innerText = 'SMS enviado com sucesso!'
+        } else {
+            alert('Erro ao enviar SMS.');
+            document.getElementById('status').innerText = 'Erro ao enviar SMS.'
+        }
+    });
+
+}
--- src/server.js
+++ src/server.js
@@ -1,32 +1,87 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const axios = require('axios');
+const twilio = require('twilio');
+const dotenv = require('dotenv')
+dotenv.config();
+
+const accountSid = process.env.AccountSID;
+const authToken = process.env.AuthToken;
+
+const client = twilio(accountSid, authToken);
+
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use(cors());
 
 app.get('/teste', (req, res) => {
-    res.status(200).send({teste:"TESTE"})
+    res.status(200).send({ teste: "TESTE" })
 });
-
+const numerosAutorizados = [ // numeros verificados no twilio
+    "+5549991868829"
+]
+const riosMonitorados = [
+    { nome: "Xanxerê", channelID: 2572787, apiKey: process.env.API_KEY },
+]
+function getRioFeed(channelID, apiKey, results = 50) {
+    return axios.get(`https://api.thingspeak.com/channels/${channelID}/feeds.json`, {
+        params: {
+            results,
+        }
+    })
+}
+function indiceRioMonitorado(nomeRio) {
+    return riosMonitorados.findIndex(rio => rio.nome === nomeRio)
+}
 //rota para envio de SMS
-app.get('/sendSMS', (req, res) => {
-
-    //implemente a lógica aqui
-
+app.get('/sendSMS', async (req, res) => { // sendSMS?nomeRio=Xanxerê
+    const indiceRio = indiceRioMonitorado(req.query.nomeRio)
+    if (indiceRio === -1) {
+        return res.status(400).send({ success: false, message: "Rio não monitorado" })
+    }
+    const channelID = riosMonitorados[indiceRio].channelID;
+    
+    const response = await getRioFeed(channelID, process.env.API_KEY, 1);
+    const altura = response.data.feeds[0].field1;
+    const dataAtual = new Date().toLocaleString();
+    const trialNumber = "+17745152897" // numero de teste do twilio
+    const body = `Alerta!! A altura da água do rio ${req.query.nomeRio} está acima do normal. Altura: ${altura}m. Data:${dataAtual}`
+    const messageSids = await Promise.all(numerosAutorizados.map(async numero => {
+        const message = await client.messages.create({
+            body,
+            to: numero,
+            from: trialNumber,
+        });
+        return message.sid;
+    }));
+    res.status(200).send({ success: true, messageSids })
 });
 
 //rota para atualizar os gráficos
-app.get('/data', async(req, res) => {
+app.get('/data', async (req, res) => { // /data?nomeRio=Xanxerê
+    const indiceRio = indiceRioMonitorado(req.query.nomeRio)
+    if (indiceRio === -1) {
+        console.log(req.query.nomeRio)
+        return res.status(400).send({ success: false, message: "Rio não monitorado" })
+    }
+    const channelID = riosMonitorados[indiceRio].channelID;
 
-    //implemente sua solução aqui
-
+    const response = await getRioFeed(channelID, process.env.API_KEY);
+    const data = response.data.feeds.map(feed => {
+        return {
+            created_at: feed.created_at,
+            value: feed.field1
+        }
+    });
+    return res.status(200).send(data);
 });
+
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
